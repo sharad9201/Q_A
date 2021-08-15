1bb7cab195cf9504a8eb02f65c8daea77957ece3
sharad9201 <sharad.rae19@gmail.com>
2021-08-15 20:33:05 +0545

    question setups

---
+++ ques_rev/src/Question.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+const Question = ({title,info}) => {
+  return <article className="question">
+
+      <header>
+          <h4>{title}</h4>
+          <button className="btn">
+              btn
+          </button>
+      </header>
+  </article>;
+};
+
+export default Question;
